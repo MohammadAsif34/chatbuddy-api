c34891290733f124382638f7132fee56bc31c8e1
MohammadAsif34 <shadowforever34@gmail.com>
2025-04-01 11:48:13 +0530

    update cors

--- server.js
+++ server.js
@@ -13,11 +13,12 @@ dotenv.config();
 const PORT = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-// app.use(
-//   cors({ origin: "https://mohammadasif34.github.io", credentials: true })
-// );
-// app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://mohammadasif34.github.io"],
+    credentials: true,
+  })
+);
 
 const connectDB = async () => {
   try {
